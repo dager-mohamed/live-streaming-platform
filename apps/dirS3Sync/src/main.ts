faaf204e0704bac64b615922841987b7387538d9
Mohamed Ashraf <55748253+dager-mohamed@users.noreply.github.com>
2024-08-14 15:48:54 +0000

    removing aws S3 completely from the project due to rate limits

--- apps/dirS3Sync/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { dirWatcherAndSyncToS3 } from './utils';
-
-function main() {
-  const pathDir = 'dist/data';
-
-  dirWatcherAndSyncToS3(pathDir);
-}
-
-main();
--- apps/dirS3Sync/src/utils/aws/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './awsS3Client';
-export * from './uploadToS3';
-export * from './deleteFromS3';
