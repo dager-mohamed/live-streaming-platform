cf83fd32631451a78c6b928b09c7158e94d7b693
Mohamed Ashraf <mohmed6002090@gmail.com>
2024-08-27 15:39:40 +0300

    delete extra stuff

--- apps/backend/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { clerkClient } from '@clerk/clerk-sdk-node';
-
-@Injectable()
-export class AppService {
-  getData() {
-    return clerkClient.users.getUserList();
-  }
-}
--- shared/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-
-export * from './components/ui/button';
-export * from './components/ui/avatar';
-export * from './components/ui/textarea';
-export * from './components/ui/badge';
